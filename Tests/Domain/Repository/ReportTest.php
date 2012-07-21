ac52a006b32eb2ffb866c4fc3e4c07a44b879a94
Halleck45 <jeanfrancois@lepine.pro>
2012-07-21 09:11:36 +0200

    test - we can find a report by a feature node

---
+++ Tests/Domain/Repository/ReportTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Hal\Bundle\BehatTools\Tests;
+
+use \Hal\Bundle\BehatTools\Domain\Repository\Report as Repo_Report;
+
+class RespositoryReportTest extends \PHPUnit_Framework_TestCase
+{
+
+    private $repo;
+
+    public function setUp()
+    {
+        $folder = __DIR__ . '/../../resources/reports/';
+        $this->repo = new Repo_Report($folder);
+    }
+
+
+    public function testRepoFindReportByItsFeature()
+    {
+
+        $feature = $this->getMock('Hal\Bundle\BehatTools\Entity\GherkinInterface');
+        $feature
+            ->expects($this->any())
+            ->method('getFile')
+            ->will($this->returnValue(__DIR__.'../../resources/features/all-correct.feature'));
+
+        $report = $this->repo->getReportByFeature($feature);
+
+        $this->assertInstanceOf('Hal\Bundle\BehatTools\Entity\ReportInterface', $report, 'Report can be found by its path');
+    }
+
+    public function testUnexistentReportIsCreatedButIsEmpty()
+    {
+
+        $feature = $this->getMock('Hal\Bundle\BehatTools\Entity\GherkinInterface');
+        $feature
+            ->expects($this->any())
+            ->method('getFile')
+            ->will($this->returnValue(__DIR__.'../../resources/features/unexistent.feature'));
+
+        $report = $this->repo->getReportByFeature($feature);
+        $this->assertInstanceOf('Hal\Bundle\BehatTools\Entity\ReportInterface', $report, 'Unexistent report is created but is empty');
+    }
+
+}
