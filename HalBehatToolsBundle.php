42de39295b21a2af2627f95bd984b118505f81a1
Halleck45 <jeanfrancois@lepine.pro>
2012-07-21 07:15:33 +0200

    Initializes bundle

---
+++ HalBehatToolsBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Hal\Bundle\BehatTools;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/*
+ * This file is part of the Behat Tools
+ * (c) 2012 Jean-François Lépine <jeanfrancois@lepine.pro>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class HalBehatToolsBundle extends Bundle
+{
+
+}
