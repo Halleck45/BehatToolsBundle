be629d36ad2189a7c65cab3972032ffceea4358b
Halleck45 <jeanfrancois@lepine.pro>
2012-07-21 07:16:17 +0200

    Implements the report domain

---
+++ Domain/Model/Report/State/Found.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Hal\Bundle\BehatTools\Model\Report\State;
+
+/*
+ * This file is part of the Behat Tools
+ * (c) 2012 Jean-François Lépine <jeanfrancois@lepine.pro>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * State of a reports : found
+ *
+ * @author Jean-François Lépine <jeanfrancois@lepine.pro>
+ */
+class Found implements StateInterface
+{
+
+    /**
+     * Content of the report
+     *
+     * @var string
+     */
+    private $reportContent;
+
+    /**
+     * XML Element
+     *
+     * @var \SimpleXMLElement
+     */
+    private $xml;
+
+    /**
+     * Constructor
+     *
+     * @param string $reportContent
+     */
+    public function __construct($reportContent)
+    {
+        $this->reportContent = (string) $reportContent;
+        $this->xml = new \SimpleXMLElement($this->reportContent);
+    }
+
+    /**
+     * Get content of the report file
+     *
+     * @return null|string
+     */
+    public function getFile()
+    {
+        return (string) $this->xml['file'];
+    }
+
+    /**
+     * Get content of the report file
+     *
+     * @return null|string
+     */
+    public function getReportContent()
+    {
+        return $this->reportContent;
+    }
+
+    /**
+     * Count errors
+     *
+     * @return null|integer
+     */
+    public function countErrors()
+    {
+        return (int) $this->xml['errors'];
+    }
+
+    /**
+     * Count failures
+     *
+     * @return null|integer
+     */
+    public function countFailures()
+    {
+        return (int) $this->xml['failures'];
+    }
+
+    /**
+     * Count tests
+     *
+     * @return null|integer
+     */
+    public function countTests()
+    {
+        return (int) $this->xml['tests'];
+    }
+
+    /**
+     * Get duration
+     *
+     * @return null|float
+     */
+    public function getDuration()
+    {
+        return (int) $this->xml['durations'];
+    }
+
+    /**
+     * List all tests cases
+     *
+     * @return array|SimpleXMLElement
+     */
+    public function listTestCases()
+    {
+        return $this->xml;
+    }
+
+}
---
+++ Domain/Model/Report/State/NotFound.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Hal\Bundle\BehatTools\Model\Report\State;
+
+/*
+ * This file is part of the Behat Tools
+ * (c) 2012 Jean-François Lépine <jeanfrancois@lepine.pro>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * State of a reports : found
+ *
+ * @author Jean-François Lépine <jeanfrancois@lepine.pro>
+ */
+class NotFound implements StateInterface
+{
+
+    /**
+     * Get content of the report file
+     *
+     * @return string
+     */
+    public function getFile()
+    {
+        return null;
+    }
+
+    /**
+     * Get content of the report file
+     *
+     * @return string
+     */
+    public function getReportContent()
+    {
+        return null;
+    }
+
+    /**
+     * Count errors
+     *
+     * @return integer
+     */
+    public function countErrors()
+    {
+        return null;
+    }
+
+    /**
+     * Count failures
+     *
+     * @return integer
+     */
+    public function countFailures()
+    {
+        return null;
+    }
+
+    /**
+     * Count tests
+     *
+     * @return integer
+     */
+    public function countTests()
+    {
+        return null;
+    }
+
+    /**
+     * Get duration
+     *
+     * @return float
+     */
+    public function getDuration()
+    {
+        return null;
+    }
+
+    /**
+     * List all tests cases
+     *
+     * @return Traversable
+     */
+    public function listTestCases()
+    {
+        return array();
+    }
+
+}
